f1c8c6c5594ad6e8622247bb086280ec8f13cb11
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-04-15 06:11:41 +0800

    unit tests

--- src/huts_system/Launcher.java
+++ src/huts_system/Launcher.java
@@ -2,6 +2,7 @@ package huts_system;
 
 import view.MainWindow;
 import java.awt.EventQueue;
+import tests.Tests;
 
 public class Launcher {
 
@@ -9,15 +10,24 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            try {
-                MainWindow main = new MainWindow();
-                main.setVisible(true);
-                System.out.println("Started");
-            } catch (Exception e) {
-                System.out.println("crashed");
-            }
-        });
+//        EventQueue.invokeLater(() -> {
+//            try {
+//                MainWindow main = new MainWindow();
+//                main.setVisible(true);
+//                System.out.println("Started");
+//            } catch (Exception e) {
+//                System.out.println("crashed");
+//            }
+//        });
+        Tests test = new Tests();
+        test.runAll();
+        if (test.getStatus()) {
+            System.out.println("Tests Passed");
+            test.printSuccesses();
+        } else {
+            System.out.println("Tests Failed");
+            test.printErrors();
+        }
     }
     
 }
