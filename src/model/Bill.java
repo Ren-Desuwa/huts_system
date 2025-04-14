170e01a571b174ba174f7a75810ec4ac51bb0daf
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-04-15 00:18:27 +0800

    test

--- src/model/Bill.java
+++ src/model/Bill.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.time.LocalDate;
 
-/**
- *
- * @author Ireneo III
- */
+
 public class Bill{
     private int id;
     private String utilityType; // "ELECTRICITY", "WATER", "GAS"
