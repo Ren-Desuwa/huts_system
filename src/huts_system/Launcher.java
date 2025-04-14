7b9a6988c5f00a927d64deb138a5ab8d1f91423c
Huuujji <Degenerate1456@gmail.com>
2025-04-15 06:10:33 +0800

    views

--- src/huts_system/Launcher.java
+++ src/huts_system/Launcher.java
@@ -1,5 +1,6 @@
 package huts_system;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import view.MainWindow;
 import java.awt.EventQueue;
 
@@ -9,6 +10,7 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
         EventQueue.invokeLater(() -> {
             try {
                 MainWindow main = new MainWindow();
